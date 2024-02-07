e1b2b510c36e19bb3eaa26643020b8a0450817c0
João pedro Maciel pinheiro <shionlk98@gmail.com>
2024-02-06 23:45:21 -0300

    teste

--- src/app/Social.tsx
+++ src/app/Social.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './css/social.css'
-import { FaLinkedin, FaWhatsappSquare } from 'react-icons/fa'
-import { FaGithub } from "react-icons/fa";
-import { MdEmail } from 'react-icons/md'
+import Image from 'next/image'
+import GitIcon from '@/utils/github.png'
+import LinkedinIcon from '@/utils/linkedin.png'
+import MailIcon from '@/utils/mensagem.png'
+import WhatsIcon from '@/utils/whatsapp.png'
+
+
 
 const Social = () => {
     return (
@@ -12,13 +16,17 @@ const Social = () => {
             <div className='footer-colum'>
                 <ul className='ul-footer'>
 
-                    <li className='link-footer'  ><a href="https://www.linkedin.com/in/jpmp1998/" target='blank'><FaLinkedin /></a>  </li>
+                    <li className='link-footer'  ><a href="https://www.linkedin.com/in/jpmp1998/" target='blank'>
+                        <Image className='icons'  src={LinkedinIcon} alt='linkedin' /></a></li>
 
-                    <li className='link-footer'  ><a href="https://github.com/Pedro-maciel-pinheiro" target='blank'><FaGithub /></a></li>
+                    <li className='link-footer'  ><a href="https://github.com/Pedro-maciel-pinheiro" target='blank'>
+                        <Image className='icons'  src={GitIcon} alt='github' /></a></li>
 
-                    <li className='link-footer'  ><a href="mailto:shionlk98@gmail.com" target='blank'><MdEmail /></a></li>
+                    <li className='link-footer'  ><a href="mailto:shionlk98@gmail.com" target='blank'>
+                        <Image className='icons'  src={MailIcon} alt='mail' />   </a></li>
 
-                    <li className='link-footer'  > <a href="https://web.whatsapp.com/send?phone=5561998516239" target="_blank"><FaWhatsappSquare /></a></li>
+                    <li className='link-footer'  > <a href="https://web.whatsapp.com/send?phone=5561998516239" target="_blank">
+                        <Image className='icons'  src={WhatsIcon} alt='whatsapp' />   </a></li>
 
                 </ul>
             </div>
